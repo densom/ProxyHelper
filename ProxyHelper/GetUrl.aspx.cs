a0bc05db07bec4969c00f338bc84d7f1dede8adb
Dennis Somerville <dennis.somerville@gmail.com>
2012-03-31 19:40:49 -0400

    Initial commit.

---
+++ ProxyHelper/GetUrl.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CM = System.Configuration.ConfigurationManager;
+
+namespace ProxyHelper
+{
+    public partial class GetUrl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Process(Context);
+        }
+
+        private void Process(HttpContext context)
+        {
+            var url = HttpUtility.UrlDecode(context.Request["url"]);
+            var proxy = new WebProxy(@"http://proxy.sherwinproxy.com", true, null, new NetworkCredential(CM.AppSettings["ProxyUser"], CM.AppSettings["ProxyPassword"]));
+
+            using (var client = new WebClient())
+            {
+                client.Proxy = proxy;
+                var data = client.DownloadString(url);
+
+                context.Response.Write(data);
+            }
+        }
+    }
+}
